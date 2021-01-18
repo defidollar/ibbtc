63f1c3c6dcf13c0d247316b3db15da000276429f
vaibhavchellani <vaibhavchellani223@gmail.com>
2021-01-18 21:54:02 +0530

    feat: use hardhat mainnet fork

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,31 +1,33 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-web3");
-require("@nomiclabs/hardhat-truffle5");
+require('@nomiclabs/hardhat-waffle')
+require('@nomiclabs/hardhat-web3')
+require('@nomiclabs/hardhat-truffle5')
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
-
+task('accounts', 'Prints the list of accounts', async () => {
+  const accounts = await ethers.getSigners()
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.address)
   }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+})
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   networks: {
-    ganache: {
+    hardhat: {
+      forking: {
+        url: 'https://eth-mainnet.alchemyapi.io/v2/<>',
+        blockNumber: 11679973,
+      },
+    },
+    local: {
       url: 'http://127.0.0.1:8545',
     },
   },
   solidity: {
-    version: "0.6.12",
-  }
-};
-
+    version: '0.6.12',
+  },
+  mocha: {
+    timeout: 200000,
+  },
+}
