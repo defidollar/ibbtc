e0133c4123025e6523e6ea48236a809674cddeec
dapp-whisperer <dapp-whisperer@pm.me>
2021-11-11 11:49:39 -0500

    add guardian to core, mint and redeem flags per peak

--- contracts/Core.sol
+++ contracts/Core.sol
@@ -11,7 +11,7 @@ import {IbBTC} from "./interfaces/IbBTC.sol";
 import {ICore} from "./interfaces/ICore.sol";
 import {GovernableProxy} from "./common/proxy/GovernableProxy.sol";
 
-contract Core is GovernableProxy, ICore {
+contract Core is GovernableProxy, Pausable, ICore {
     using SafeERC20 for IERC20;
     using SafeMath for uint;
     using Math for uint;
@@ -22,7 +22,7 @@ contract Core is GovernableProxy, ICore {
 
     BadgerGuestListAPI public guestList;
 
-    enum PeakState { Extinct, Active, Dormant }
+    enum PeakState { Extinct, Active, RedeemOnly, MintOnly }
     mapping(address => PeakState) public peaks;
 
     address[] public peakAddresses;
@@ -31,7 +31,9 @@ contract Core is GovernableProxy, ICore {
     uint public redeemFee;
     uint public accumulatedFee;
 
-    uint256[50] private __gap;
+    address public guardian;
+
+    uint256[49] private __gap;
 
     // END OF STORAGE VARIABLES
 
@@ -46,6 +48,11 @@ contract Core is GovernableProxy, ICore {
         bBTC = IbBTC(_bBTC);
     }
 
+    modifier onlyGuardianOrGovernance() {
+        require(msg.sender == guardian || msg.sender == governance, "onlyGuardianOrGovernance");
+        _;
+    }
+
     /**
     * @notice Mint bBTC
     * @dev Only whitelisted peaks can call this function
@@ -55,9 +62,10 @@ contract Core is GovernableProxy, ICore {
     function mint(uint btc, address account, bytes32[] calldata merkleProof)
         override
         external
+        whenNotPaused
         returns(uint)
     {
-        require(peaks[msg.sender] == PeakState.Active, "PEAK_INACTIVE");
+        require(peaks[msg.sender] == PeakState.Active || peaks[msg.sender] == PeakState.MintOnly, "PEAK_INACTIVE_OR_MINTING_DISABLED");
         if (address(guestList) != address(0)) {
             require(
                 guestList.authorized(account, btc, merkleProof),
@@ -91,9 +99,9 @@ contract Core is GovernableProxy, ICore {
     * @param bBtc bBTC amount to redeem
     * @return btc amount redeemed, scaled by 1e36
     */
-    function redeem(uint bBtc, address account) override external returns (uint) {
+    function redeem(uint bBtc, address account) override external whenNotPaused returns (uint) {
         require(bBtc > 0, "REDEEMING_0_bBTC");
-        require(peaks[msg.sender] != PeakState.Extinct, "PEAK_EXTINCT");
+        require(peaks[msg.sender] == PeakState.Active || peaks[msg.sender] == PeakState.RedeemOnly, "PEAK_INACTIVE_OR_REDEMPTION_DISABLED");
         (uint btc, uint fee) = bBtcToBtc(bBtc);
         accumulatedFee = accumulatedFee.add(fee);
         bBTC.burn(account, bBtc);
@@ -119,7 +127,7 @@ contract Core is GovernableProxy, ICore {
     /**
     * @notice Collect all the accumulated fee (denominated in bBTC)
     */
-    function collectFee() external {
+    function collectFee() external whenNotPaused {
         require(feeSink != address(0), "NULL_ADDRESS");
         uint _fee = accumulatedFee;
         require(_fee > 0, "NO_FEE");
@@ -142,6 +150,13 @@ contract Core is GovernableProxy, ICore {
     }
 
     /* ##### Governance ##### */
+    function pause() onlyGuardianOrGovernance {
+        _pause();
+    }
+
+    function unpause() onlyGovernance {
+        unpause();
+    }
 
     /**
     * @notice Whitelist a new peak
@@ -213,6 +228,18 @@ contract Core is GovernableProxy, ICore {
 
     function setGuestList(address _guestList) external onlyGovernance {
         guestList = BadgerGuestListAPI(_guestList);
+    }
+
+    function setGuardian(address _guardian) external onlyGovernance {
+        guardian = _guardian;
+    }
+
+    function pause() onlyGuardianOrGovernance {
+        _pause();
+    }
+
+    function unpause() onlyGovernance {
+        _unpause();
     }
 }
 
